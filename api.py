91ed2d31202cb2d1d561768cc2cc441dbf50e4ee
bhavanishankar2005 <bhavanishankarsambu@gmail.com>
2026-07-04 10:55:38 +0530

    Add deployment files for Railway

--- api.py
+++ api.py
@@ -1039,4 +1039,5 @@ def admin_reject_product(pid):
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
